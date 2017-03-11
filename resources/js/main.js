dcfa2a85587f3e0bab285c1b428746a1e556b771
decanter <theeasterlywood@gmail.com>
2017-03-11 22:07:19 +0000

    fixed map, work on button presses

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,18 +1,16 @@
 function buildMap(){
-        var mapOptions = {
-            center: new google.maps.LatLng(57.149717,-2.094278),
-            zoom: 10,
-            mapTypeId: google.maps.MapTypeId.TERRAIN
-        }
-        var map = new google.maps.Map(document.getElementById("google-map"), mapOptions);
+    var mapOptions = {
+        center: new google.maps.LatLng(57.149717,-2.094278),
+        zoom: 10,
+        mapTypeId: google.maps.MapTypeId.TERRAIN
+    };
+    var map = new google.maps.Map(document.getElementById("google-map"), mapOptions);
 }
 
 function buttonPress(){
     for (var i = 0 ; i < imageArray.length ; ++i) {
-        function(index) {
-            imageArray[index].addEventListener("click", function() {
+            imageArray[i].addEventListener("click", function() {
                 alert ("You clicked region number: " + index);
             });
-        } ( i);
     }
 }
