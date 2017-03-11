6f5a3e7f6e4689e5e4a0dad1e206484aff11e426
decanter <theeasterlywood@gmail.com>
2017-03-11 23:53:51 +0000

    finished mockup button presses

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,16 +1,15 @@
-function buildMap(){
-    var mapOptions = {
+var button_pressed_id = "headerBtnWalk";
+
+function build_map(){
+    var map_options = {
         center: new google.maps.LatLng(57.149717,-2.094278),
         zoom: 10,
-        mapTypeId: google.maps.MapTypeId.TERRAIN
-    };
-    var map = new google.maps.Map(document.getElementById("google-map"), mapOptions);
+        mapTypeId: google.maps.MapTypeId.TERRAIN};
+    var map = new google.maps.Map(document.getElementById("google-map"), map_options);
 }
 
-function buttonPress(){
-    for (var i = 0 ; i < imageArray.length ; ++i) {
-            imageArray[i].addEventListener("click", function() {
-                alert ("You clicked region number: " + index);
-            });
-    }
+function button_press_event(button_pressed){
+    button_pressed_id.className = 'headerBtn' + 'Off';
+    button_pressed_id = document.getElementById(button_pressed);
+    button_pressed_id.className = 'headerBtn' + ( ~button_pressed_id.className.indexOf('On', 8) ? 'Off' : 'On');
 }
