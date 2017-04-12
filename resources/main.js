65d80cfe5fa00b0ecfa8e21ed26e0305b89e889e
decantr <theeasterlywood@gmail.com>
2017-04-12 02:23:06 +0100

    commented the new changes

--- resources/main.js
+++ resources/main.js
@@ -1,15 +1,15 @@
-var infoButtons = "<br><p>is this the correct place?<p><br>\
-                    <button class='button welcome_info_button' id='yes'>yes</button>\
-                    <button class='button welcome_info_button' id='no'>no</button>",
-    welcome_input_boxes_arraya,
+// array to hold our two input boxes
+var welcome_input_boxes_arraya,
+    // global tracker for determining which box to add the selected location to
     welcome_which_map,
+    // global tracker for the marker
+    welcome_marker = null,
     // array to hold all of our location selection boxes
     welcome_input_boxes_array = [],
     // array to hold all of our transport buttons
     results_transport_buttons_array,
-    // array for the place_id's that were selected
-    results_location_array = [],
-    welcome_markers = null;
+    // array for the locations that were selected
+    results_location_array = [];
 
 // generate the array of buttons and add click events to each of them
 function generate_array() {
@@ -31,6 +31,7 @@ function generate_array() {
             results_background_change(i);
         });
     }
+    // select the centre button (temp)
     results_transport_buttons_array[2].click();
 }
 
@@ -78,47 +79,81 @@ function build_map() {
         // set the type of map
         mapTypeId: google.maps.MapTypeId.TERRAIN
     };
+    // set up the geocoder that will turn coords into locations
     var geocoder = new google.maps.Geocoder,
+        // set the info window object that will be attached to the marker
         welcome_info_window = new google.maps.InfoWindow,
         // make the google map
         map = new google.maps.Map(document.getElementById("google_map"), map_options);
+    // add an event listener to the map to get the location
     google.maps.event.addListener(map, 'click', function (event) {
-        if (welcome_markers) welcome_markers.setMap(null);
-        geocodeLatLng(geocoder, map, welcome_info_window, event.latLng);
+        // if there is a marker there, remove it
+        if (welcome_marker) welcome_marker.setMap(null);
+        // call the function the handle the location
+        welcome_geocode_location(geocoder, map, welcome_info_window, event.latLng);
     });
 }
 
-function geocodeLatLng(geocoder, map, welcome_info_window, location) {
+// function to get work out the location and handle creating the info window
+function welcome_geocode_location(geocoder, map, welcome_info_window, location) {
+    // call the geocode api
     geocoder.geocode({ 'location': location }, function (results, status) {
-        var welcome_markers = new google.maps.Marker({
+        // make our marker
+        welcome_marker = new google.maps.Marker({
+            // set the position to the location clicked
             position: location,
+            // set the map to our map
             map: map
         });
+        // if the geocoding returns successfully 
         if (status === 'OK') {
-            welcome_info_window.setContent(results[0].formatted_address + infoButtons);
-            welcome_info_window.open(map, welcome_markers);
+            // set the contents of our info window to the address
+            welcome_info_window.setContent(results[0].formatted_address +
+                // add some text and two buttons for choosing
+                "<br><p>is this the correct place?<p><br>\
+                <button class='button welcome_info_button' id='yes'>yes</button>\
+                <button class='button welcome_info_button' id='no'>no</button>");
+            // open the info window on the marker this has to be here otherwise we cant access the buttons
+            welcome_info_window.open(map, welcome_marker);
+            // create an array of the two buttons on the info window we just made
             var welcome_info_buttons = document.getElementsByClassName('welcome_info_button');
+            // iterate through the array
             for (let i = 0; i < welcome_info_buttons.length; i++) {
+                // add an event listener to each
                 welcome_info_buttons[i].addEventListener('click', function () {
-                    if (welcome_info_buttons[i].id === 'yes') welcome_info_button_press(welcome_info_buttons[i], welcome_markers, welcome_info_window, results[1].formatted_address);
-                    welcome_markers.setMap(null);
+                    // the if the id of the button is 'yes' then call the function to handle setting the location
+                    if (welcome_info_buttons[i].id === 'yes') welcome_info_button_press(welcome_info_buttons[i], welcome_marker, welcome_info_window, results[1].formatted_address);
+                    // then remove the marker
+                    welcome_marker.setMap(null);
                 });
             }
+            // if the status return as over query limit then tell the user
         } else if (status === 'OVER_QUERY_LIMIT') welcome_info_window.setContent('<h2>slow down!</h2><h4>too many requests</h4>');
+        // else if it returns as no results found then tell the user
         else if (status === 'ZERO_RESULTS') welcome_info_window.setContent('<h3>sorry no results found!</h3><h1>:(</h1>');
+        // if there is something else returned panic
         else welcome_info_window.setContent('<h3>something has gone terribly wrong</h3>');
-        welcome_info_window.open(map, welcome_markers);
+        // ensure the info window is opened
+        welcome_info_window.open(map, welcome_marker);
     });
 }
 
-function welcome_info_button_press(btn, welcome_markers, info, loc) {
+// function to set the contents of the input boxes and hide our map when we finished
+function welcome_info_button_press(btn, welcome_marker, info, loc) {
+    // if the map was opened from the start box
     if (welcome_which_map == 'start') {
-        welcome_input_boxes_arraya[0].value = '' + loc;
+        // set the value of the input box to the location returned by the reverse geocode
+        welcome_input_boxes_arraya[0].value = loc;
+        // set the same information into our array for accessing later
         results_location_array[0] = loc;
+        // else if the map was opened from the destination box
     } else {
+        // set the value of the input box to the location returned by the reverse geocode
         welcome_input_boxes_arraya[1].value = '' + loc;
+        // set the same information into our array for accessing later
         results_location_array[1] = loc;
     }
+    // close the map
     document.getElementById("google_map").style.zIndex = '-2';
 }
 
@@ -134,25 +169,31 @@ function closeNav() {
     document.getElementById("results_bar").style.width = "0";
 }
 
-// function to set the input boxes to integrate with places api
+// function to make the input boxes to integrate with places api
 function welcome_load() {
+    // fill our array of input boxes
     welcome_input_boxes_arraya = document.getElementsByClassName('welcome_box_input_box');
-    var options = { types: ['(cities)'] };
-
+    // iterate through the array of input boxes
     for (let i = 0; i < welcome_input_boxes_arraya.length; i++)
-        welcome_input_boxes_array[i] = new google.maps.places.Autocomplete(welcome_input_boxes_arraya[i], options);
+        // at each add the autocomplete api with the cities constraint 
+        welcome_input_boxes_array[i] = new google.maps.places.Autocomplete(welcome_input_boxes_arraya[i], { types: ['(cities)'] });
 }
 
 // function to get the inputs from the buttons
 function welcome_inputs_selected() {
+    // iterate through our array of buttons we made earlier
     for (let i = 0; i < welcome_input_boxes_array.length; i++) {
+        // if the contents of the array has not already been filled from the map
         if (results_location_array[i])
+            // then add the location from the input box
             results_location_array[i] = welcome_input_boxes_array[i].getPlace();
     }
 }
 
 // function to dispaly the map on button press
 function welcome_show_map(who) {
+    // set our global variable to the button presseer
     welcome_which_map = who;
+    // show our map (this is a temporary way of doing this i promise)
     document.getElementById("google_map").style.zIndex = '100';
 }
